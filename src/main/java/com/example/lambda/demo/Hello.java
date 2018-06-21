d21c5775d4b5cc97b042bb4812d43f60b4667098
unknown <seifu001@umn.edu>
2018-06-21 10:21:31 -0700

    init

---
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.example.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<RequestClass, ResponseClass> {
+
+    @Override
+    public ResponseClass handleRequest(RequestClass request, Context context){
+        String greetingString = String.format("Hello %s, %s.", request.firstName, request.lastName);
+        return new ResponseClass(greetingString);
+    }
+    
+
+}
---
+++ src/main/java/com/example/lambda/demo/ResponseClass.java
@@ -0,0 +1,20 @@
+package com.example.lambda.demo;
+
+public class ResponseClass {
+	String greetings;
+
+    public String getGreetings() {
+        return greetings;
+    }
+
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
+    }
+
+    public ResponseClass(String greetings) {
+        this.greetings = greetings;
+    }
+
+    public ResponseClass() {
+    }
+}
